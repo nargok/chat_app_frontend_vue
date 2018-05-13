26df49e874a290fceb645ebd67903621bcb31d68
Ryoichi Gokan <nargok@gmail.com>
2018-05-13 22:40:18 +0900

    component message_list & css modification

--- src/components/MessageList/index.vue
+++ src/components/MessageList/index.vue
@@ -1,3 +1,3 @@
-<!-- <template src="./message_list.pug" lang="pug" /> -->
-<template src="./message_list.html"　/>
+<template src="./message_list.pug" lang="pug" />
+<!--　<template src="./message_list.html"　/> -->
 <script src="./message_list.js" />
